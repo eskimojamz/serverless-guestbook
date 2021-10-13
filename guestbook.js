a38f6782cd466b4756d9d30dce116add43719eaf
James Kim <jameskim917@gmail.com>
2021-10-13 15:24:12 +0900

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://22524e72.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
